97b21c6834c86a8f92325e3e50b1b3c26afaa0cd
Aleksandr Glebov <89081349143@mail.ru>
2024-04-28 01:11:32 -0600

    clear app

--- licensure-app/src/main.tsx
+++ licensure-app/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+// this manifest is used temporarily for development purposes
+const manifestUrl =
+  "https://raw.githubusercontent.com/Licensure-TMA/frontend/main/tonconnect-manifest.json";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+  </TonConnectUIProvider>
+);
